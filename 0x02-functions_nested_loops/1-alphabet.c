b720f6c02f2bee8bd95150239b36d17d4214c5e0
yome fisseha <yomefisseha203@gmail.com>
2022-07-06 09:03:01 -0700

    alphabet geme

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * print_alphabt - prints alphabet
+ */
+void print_alphabet(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void print_alphabet(void);
