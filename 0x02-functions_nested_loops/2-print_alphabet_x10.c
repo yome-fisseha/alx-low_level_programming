64e49b65ffb1c6179cfc7a61dff578c1844eaa47
yome fisseha <yomefisseha203@gmail.com>
2022-07-06 11:28:32 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - prints alphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i=0;
+	for(i=0; i<10; i++)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,7 @@
  * print_alphabet - prints alphabet
  */
 void print_alphabet(void);
+/**
+ * print_alphabet_x10 - print alphabet 10 times 
+ */
+void print_alphabet_x10(void);
